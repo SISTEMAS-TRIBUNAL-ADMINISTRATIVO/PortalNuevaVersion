c7c20742e81b66c4823cc1382017dbee7ac6b952
ServicioSocial1-TAPJE <serviciosocial01tapje@gmail.com>
2025-01-15 13:07:51 -0600

    base de datos nuevas

--- Database/14_01_2025_Add_Pri_notification.sql
+++ Database/14_01_2025_Add_Pri_notification.sql
@@ -1,14 +1,15 @@
 SET NAMES utf8mb4;
 SET COLLATION_CONNECTION = 'utf8mb4_spanish2_ci';
 
-CREATE TABLE portal_empleado.pri_notificacion (
+CREATE TABLE pri_notificacion 
+(
     idnotificacion INT(10) AUTO_INCREMENT PRIMARY KEY,
     fk_tipo_notificacion INT(10),
     mensaje_notificacion TEXT,
     imagen_notificacion LONGBLOB,
-    usuario_se_notifica INT(10),
+    fk_usuario_se_notifica INT(10),
     fk_area INT(10),
     fecha_notificacion DATE,
-    FOREIGN KEY (fk_tipo_notificacion) REFERENCES cat_tipo_notificacion(idtiponotificacion),
-    FOREIGN KEY (fk_area) REFERENCES cat_area(idarea)
+    estado_notificacion int(10)
+    FOREIGN KEY (fk_tipo_notificacion) REFERENCES cat_tipo_notificacion(idtiponotificacion)
 );
