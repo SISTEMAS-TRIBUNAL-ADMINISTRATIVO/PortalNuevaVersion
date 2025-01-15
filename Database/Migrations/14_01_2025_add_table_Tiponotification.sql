3d1c081e6ceeb026c34a6b0000851f6cc8c1d2a1
SISTEMAS TRIBUNAL ADMINISTRATIVO <sistemastapje@hotmail.com>
2025-01-15 15:59:18 -0600

    se trabaja con scritp para las migraciones

---
+++ Database/Migrations/14_01_2025_add_table_Tiponotification.sql
@@ -0,0 +1,7 @@
+CREATE TABLE cat_tipo_notificacion 
+(
+    idtiponotificacion INT(10) AUTO_INCREMENT PRIMARY KEY,
+    Nombre_notificcion VARCHAR(100) NOT NULL,
+    comentarios_tiponotificacion VARCHAR(100) NOT NULL
+);
+
---
+++ Database/Migrations/14_01_2025_insert_tipoNotificacion.sql
@@ -0,0 +1,6 @@
+INSERT INTO cat_tipo_notificacion (Nombre_notificcion, comentarios_tiponotificacion)
+VALUES 
+('Notificación a todo el personal', 'Notificación enviada a todos los empleados'),
+('Notificación por áreas', 'Notificación enviada por áreas específicas'),
+('Felicitaciones por cumpleaños', 'Notificación para felicitar a los empleados en su cumpleaños'),
+('Notificación personal directo', 'Notificación enviada a un empleado de forma directa');
